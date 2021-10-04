8131ff4f7b1d40494c3beb300802b9097703b5ab
jhuancho <84026547+gitjhuanjulio@users.noreply.github.com>
2021-10-04 22:14:48 +0800

    Updated

---
+++ script.js
@@ -0,0 +1,42 @@
+const form = document.getElementById('form')
+// const submit = document.querySelector('button')
+
+// submit.addEventListener('click', function(e){
+//     const submitBtn = e.currentTarget.parentElement;
+//     submitBtn.classList.toggle('show-error')
+
+//     e.preventDefault()
+// })
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    
+    mainInput();
+})
+
+function mainInput() {
+    const emailValue = email.value.trim()
+
+    if(email.value === '' || email.value == null) {
+        setErrorFor(email, 'Email cannot be empty')
+    } else {
+        setSuccessFor(email)
+    }
+
+}
+
+function setErrorFor(input, message) {
+    const formControl = input.parentElement // .form-control
+    const small = formControl.querySelector('small');
+
+    // add error message inside small
+    small.innerText = message;
+
+    // add error class
+    formControl.className = 'form-control error'
+}
+
+function setSuccessFor(input) {
+    const formControl = input.parentElement
+    formControl.className = 'form-control success'
+}
